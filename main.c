a4357a81c9a65348dc54cf290d1b5ea5ac316640
MHAllen01 <mhallen@pugetsound.edu>
2023-03-07 09:43:15 +0000

    Fixed clocking errors

--- main.c
+++ main.c
@@ -25,13 +25,15 @@ int main(int argc, char *argv[]) {
 					// Run the program 4 times
 					mmm_init();
 					// Run the sequential program
-					clockstart = rtclock();
 					mmm_seq();
-					clockend = rtclock(); // stop clocking
 					// Reset the matrices
 					mmm_reset(inputMatrix1);
 					mmm_reset(inputMatrix2);
 					mmm_reset(outputMatrixSeq);
+
+					// Initialize the clock
+					clockstart = 0;
+					clockend = 0;
 
 					for (int i=0; i<3; i++) {
 						// Initialize and populate the two matrices
@@ -64,7 +66,7 @@ int main(int argc, char *argv[]) {
 				}
 			} else {
 				// Error
-				printf("\nNot enough parameters. Try ./mmm S <size of matrices>\n");
+				printf("\nIncorrect parameters. Try ./mmm S <size of matrices>\n");
 			}
 
 		} else if (strcmp(argv[1], "P") == 0) {
@@ -76,66 +78,32 @@ int main(int argc, char *argv[]) {
 				// Valid number of threads
 				if (*check == '\0' || check != argv[3]) {
 					size = strtod(argv[2], &check);
+					
 					// Valid size
 					if (*check == '\0' || check != argv[2]) {
 						size = strtod(argv[3], &check);
 
-						// Prepare thread arguments
-						thread_args *args = (thread_args*) malloc(sizeof(thread_args) * numThreads);
+						// Valid threads
+						if (*check == '\0' || check != argv[3]) {
+							if (numThreads <= size) {
+								// Prepare thread arguments
+								thread_args *args = (thread_args*) malloc(sizeof(thread_args) * numThreads);
 
-						for (int i=0; i<numThreads; i++){
-							args[i].tid = i;
-							args[i].start = i * size / numThreads + 1;
-							args[i].end = (i + 1) * size / numThreads;
-						}
+								for (int i=0; i<numThreads; i++){
+									args[i].tid = i;
+									args[i].start = i * size / numThreads + 1;
+									args[i].end = (i + 1) * size / numThreads;
+								}
 
-						// // Allocate space to hold threads
-						pthread_t *threads = (pthread_t*) malloc(sizeof(pthread_t) * numThreads);
+								// Allocate space to hold threads
+								pthread_t *threads = (pthread_t*) malloc(sizeof(pthread_t) * numThreads);
 						
-						// Initialize the matrices
-						mmm_init(); 
-
-						// Used to calculate the verification of the matrices
-						double difference = 0.0;
-
-						// Start Parallel clock
-						clockstartPar = rtclock();
-						for (int i=0; i<numThreads; i++) {
-							// Create the threads
-							pthread_create(&threads[i], NULL, mmm_par, &args[i]);
-						}
-
-						// Join the threads
-						for (int i=0; i<numThreads; i++) {
-							// Join the threads
-							pthread_join(threads[i], NULL);
-						}
-
-						// End Parallel clock
-						clockendPar = rtclock();
-
-						// Run the sequential version
-						clockstartSeq = rtclock();
-						mmm_seq();
-						clockendSeq = rtclock();
-
-						// Compare the two outputs
-						difference += mmm_verify();
-
-						// Reset all arrays
-						mmm_reset(inputMatrix1);
-						mmm_reset(inputMatrix2);
-						mmm_reset(outputMatrixSeq);
-						mmm_reset(outputMatrixPar);
-
-							// Run each 3 times and compare
-							for (int i=0; i<3; i++) {
 								// Initialize the matrices
 								mmm_init(); 
 
+								// Used to calculate the verification of the matrices
+								double difference = 0.0;
 
-								// Start Parallel clock
-								clockstartPar += rtclock();
 								for (int i=0; i<numThreads; i++) {
 									// Create the threads
 									pthread_create(&threads[i], NULL, mmm_par, &args[i]);
@@ -147,13 +115,8 @@ int main(int argc, char *argv[]) {
 									pthread_join(threads[i], NULL);
 								}
 
-								// End Parallel clock
-								clockendPar += rtclock();
-
 								// Run the sequential version
-								clockstartSeq += rtclock();
 								mmm_seq();
-								clockendSeq += rtclock();
 
 								// Compare the two outputs
 								difference += mmm_verify();
@@ -163,34 +126,82 @@ int main(int argc, char *argv[]) {
 								mmm_reset(inputMatrix2);
 								mmm_reset(outputMatrixSeq);
 								mmm_reset(outputMatrixPar);
+
+								// Initialize the clocks
+								clockstartPar = 0.0;
+								clockstartSeq = 0.0;
+								clockendPar = 0.0;
+								clockendSeq = 0.0;
+
+								// Run each 3 times and compare
+								for (int i=0; i<3; i++) {
+									// Initialize the matrices
+									mmm_init(); 
+
+									// Start Parallel clock
+									clockstartPar += rtclock();
+									for (int i=0; i<numThreads; i++) {
+										// Create the threads
+										pthread_create(&threads[i], NULL, mmm_par, &args[i]);
+									}
+
+									// Join the threads
+									for (int i=0; i<numThreads; i++) {
+										// Join the threads
+											pthread_join(threads[i], NULL);
+									}
+
+									// End Parallel clock
+									clockendPar += rtclock();
+
+									// Run the sequential version
+									clockstartSeq += rtclock();
+									mmm_seq();
+									clockendSeq += rtclock();
+
+									// Compare the two outputs
+									difference += mmm_verify();
+
+									// Reset all arrays
+									mmm_reset(inputMatrix1);
+									mmm_reset(inputMatrix2);
+									mmm_reset(outputMatrixSeq);
+									mmm_reset(outputMatrixPar);
+								}
+
+								// Free the matrices
+								mmm_freeup();
+
+								// Free threads array
+								free(threads);
+								threads = NULL;
+								free(args);
+								args = NULL;
+
+
+								// Used for time calculations
+								double avgTimeSeq = (clockendSeq - clockstartSeq) / 3.0;
+								double avgTimePar = (clockendPar - clockstartPar) / 3.0;
+								double speedup = avgTimeSeq / avgTimePar;
+
+								// Print outputs
+								printf("\n========\n");
+								printf("mode: parallel\n");
+								printf("thread count: %d\n", numThreads);
+								printf("size: %d\n", size);
+								printf("========\n");
+								printf("Sequential Time (avg of 3 runs): %.6f sec\n", avgTimeSeq);
+								printf("Parallel Time (avg of 3 runs): %.6f sec\n", avgTimePar);
+								printf("Speedup: %.6f\n", speedup);
+								printf("Verifying... largest error between parallel and sequential matrix: %.6f\n", difference);
+							} else {
+								// Error
+								printf("\nAmount of threads cannot be greater than the size!\n");
+							}
+						} else {
+							// Error
+							printf("\n%s is not a valid size.\n", argv[3]);
 						}
-
-						// Free the matrices
-						mmm_freeup();
-
-						// Free threads array
-						free(threads);
-						threads = NULL;
-						free(args);
-						args = NULL;
-
-
-						// Used for time calculations
-						double avgTimeSeq = (clockendSeq - clockstartSeq) / 3.0;
-						double avgTimePar = (clockendPar - clockstartPar) / 3.0;
-						double speedup = avgTimeSeq / avgTimePar;
-
-						// Print outputs
-						printf("\n========\n");
-						printf("mode: parallel\n");
-						printf("thread count: %d\n", numThreads);
-						printf("size: %d\n", size);
-						printf("========\n");
-						printf("Sequential Time (avg of 3 runs): %.6f sec\n", avgTimeSeq);
-						printf("Parallel Time (avg of 3 runs): %.6f sec\n", avgTimePar);
-						printf("Speedup: %.6f\n", speedup);
-						printf("Verifying... largest error between parallel and sequential matrix: %.6f\n", difference);
-
 					} else {
 						// Error
 						printf("\n%s is not a valid size.\n", argv[2]);
@@ -202,11 +213,11 @@ int main(int argc, char *argv[]) {
 				}
 			} else {
 				// Error
-				printf("\nNot enough parameters. Try ./mmm P <# of threads> <size of matrices>\n");
+				printf("\nIncorrect parameters. Try ./mmm P <# of threads> <size of matrices>\n");
 			}
 		}
 	} else {
-		printf("\nNot enough parameters. Try ./mmm <P|S>\n");
+		printf("\nIncorrect parameters. Try ./mmm <P|S>\n");
 	}
 	return 0;
 }
